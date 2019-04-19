079697d6bd1a949da73b9e80db6e05880d18e08a
CaityHeath <caity.heath@gmail.com>
2019-04-19 15:13:36 -0700

    working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
-const magnets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' © Caity Heath'];
+const magnets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' © CH'];
 
-const colors = ['#FF0000', '#0000FF', '#FFFF00', '#7FFF00', '#FF7F00', '#7F00FF'];
+const colors = ['#FF0000', '#0000FF', '#FFFF00', '#ADFF9E', '#FF7F00', '#7F00FF'];
 
 const dropZone = document.getElementById('zone');
 const body = document.getElementsByTagName('body');
@@ -27,7 +27,7 @@ function createMagnets() {
 
     let tag = addElement('p', idx, zone)
     let randomColor = Math.floor(Math.random() * 6);
-    tag.setAttribute('style', `position: absolute; left: ${x}%; top: ${y}%; padding: 3px; color:${colors[randomColor]};`);
+    tag.setAttribute('style', `position: absolute; left: ${x}%; top: ${y}%; padding: 0px; color:${colors[randomColor]};`);
     tag.addEventListener('dragstart', dragstart_handler);
   });
 }
@@ -65,8 +65,7 @@ function grabMagnet(e, data) {
   let id = data;
   Magnet.allMagnets.forEach(idx => {
     if (idx.magnetName === id) {
-      console.log(e.pageX);
-      console.log(e.pageY);
+
       idx.posx = - e.pageX;
       idx.posy = - e.pageY;
       return;
@@ -89,14 +88,12 @@ function drop_handler(e) {
   e.preventDefault();
   let data = e.dataTransfer.getData('text/plain');
   let selected = document.getElementById(data);
-  console.log(selected);
   e.target.appendChild(document.getElementById(data));
   selected.style.position = 'fixed';
-  console.log('e.pagex', e.pageX);
+  console.log(selected.offsetHeight);
+  console.log(selected.offsetWidth);
   selected.style.left = `${e.pageX}px`;
   selected.style.top = `${e.pageY}px`;
-
-  console.log('selected', selected);
   grabMagnet(e, data);
 }
 
