f6fda53f95c718ddbdde86989ec9449a65387a31
CaityHeath <caity.heath@gmail.com>
2019-04-25 15:39:37 -0700

    added a drag and drop example

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Context from './Context';
+
+
+class App extends Component {
+
+  render(){
+    return (
+      <Context />
+    )
+  }
+
+}
+
+export default App;
---
+++ src/Target.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { DropTarget } from 'react-dnd';
+
+function collect(connect, monitor){
+  return {
+    connectDropTarget: connect.dropTarget(),
+    hovered: monitor.isOver(),
+    item: monitor.getItem(),
+  }
+}
+
+
+class Target extends Component{
+  render() {
+    const { connectDropTarget, hovered, item } = this.props;
+    const backgroundColor = hovered ? 'lightgreen' : 'white';
+
+    return connectDropTarget(
+      <div className="target" style={{ background: backgroundColor, width: 400, height: 400 }}>
+        Target
+      </div>
+    )
+  }
+}
+
+export default DropTarget('item', {}, collect)(Target);
