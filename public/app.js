2bb25494c88895ed88a20805a52911c9aaf30dd2
CaityHeath <caity.heath@gmail.com>
2019-05-13 16:08:33 -0700

    px converted to vw and vh

--- public/app.js
+++ public/app.js
@@ -5,6 +5,7 @@ const colors = ['#FF0000', '#403383', '#098571', '#F8e100'];
 
 const dropZone = document.getElementById('zone');
 
+Magnet.allMagnets = [];
 
 function Magnet(mag, x, y) {
   this.magnetName = mag;
@@ -14,25 +15,34 @@ function Magnet(mag, x, y) {
   Magnet.allMagnets.push(this);
 }
 
-Magnet.allMagnets = [];
-
-
 function createMagnets() {
   magnets.forEach(idx => {
-    let x = rando(5, 95);
-    let y = rando(5, 95);
-
+    let x = rando(5, 85);
+    let y = rando(5, 85);
+    let color = rando(0, 3);
     new Magnet(idx, x, y);
 
     let tag = addElement('p', idx, zone);
-    tag.setAttribute('style', `position: absolute; left: ${x}%; top: ${y}%;`);
-    // tag.addEventListener('dragstart', dragstart_handler);
+    tag.setAttribute('style', `position: absolute; left: ${x}vw; top: ${y}vh; color:${colors[color]}`);
   });
 }
 
 $( function() {
   $( ".draggable" ).draggable();
 } );
+
+$( function() {
+  $("#zone").droppable();
+})
+
+$("#zone").on("drop", function(event, ui){
+  let id = `#${ui.draggable[0].id}`;
+  let x = calcRelativeWidth(ui.position.left);
+  let y = calcRelativeHeight(ui.position.top);
+
+  $(ui.draggable[0]).css('top', `${y}vh`);
+  $(ui.draggable[0]).css('left', `${x}vw`);
+});
 
 
 /////////// HELPER FUNCTIONS \\\\\\\\\\\\\\\\
@@ -42,77 +52,29 @@ function addElement(ele, content, parent) {
   let newContent = document.createTextNode(content);
   let newId = document.createAttribute('id');
   let newClass = document.createAttribute('class');
-
-
   newId.value = content;
   newClass.value = "draggable";
-
   newEl.setAttributeNode(newId);
   newEl.setAttributeNode(newClass);
   newEl.appendChild(newContent);
   parent.appendChild(newEl);
-
   return newEl;
 }
 
-
-
 function rando(min, max) {
   let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
   return randomNumber;
 }
 
-function grabMagnet(e, data) {
-  e.preventDefault();
-  let id = data;
-  Magnet.allMagnets.forEach(idx => {
-    if (idx.magnetName === id) {
-
-      idx.posx = - e.pageX;
-      idx.posy = - e.pageY;
-      return;
-    }
-  });
+function calcRelativeWidth(posX){
+let calcX = 100 / document.documentElement.clientWidth;
+return calcX * posX;
 }
 
-
-////////// Handlers \\\\\\\\\\\\\\\\\\\\
-function dragstart_handler(e) {
-  e.dataTransfer.setData('text/plain', e.target.id);
+function calcRelativeHeight(posY){
+let calcY = 100/ document.documentElement.clientHeight;
+return calcY * posY;
 }
-
-function dragover_handler(e) {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = 'move';
-};
-
-function drop_handler(e) {
-  e.preventDefault();
-  let data = e.dataTransfer.getData('text/plain');
-  let selected = document.getElementById(data);
-  e.target.appendChild(document.getElementById(data));
-  selected.style.position = 'fixed';
-
-  let calcX = 100 / document.documentElement.clientWidth;
-  let calcY = 100 / document.documentElement.clientHeight;
-  let randomColor = Math.floor(Math.random() * 4);
-  let x = e.pageX * calcX;
-  let y =  e.pageY * calcY;
-  //console.log('client height',document.documentElement.clientHeight);
-  selected.style.color = `${colors[randomColor]}`;
-  selected.style.left = `${x}vw`;
-  selected.style.top = `${y}vh`;
-  selected.style.padding = `${calcX * 1}vw`;
-  grabMagnet(e, data);
-}
-
-
 
 
 createMagnets();
-//dropZone.addEventListener('dragover', dragover_handler);
-
-
-// dropZone.addEventListener('drop', drop_handler);
-// dropZone.addEventListener('touchend', drop_handler);
